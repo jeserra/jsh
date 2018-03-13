972aece7ff565bea8fc589553bed51fcd79086c2
Isaac <DMDovahkiin@gmail.com>
2018-03-13 12:59:52 -0600

    Main navigation is recreated

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer stateless hide-overlay :mini-variant.sync="mini" v-model="drawer">
+      <v-toolbar flat class="transparent" v-on:mouseover="mini = false" v-on:mouseleave="mini = true">
+
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <!--
+                defaultImage: "./../../static/404.png",
+              -->
+              <img src="./../static/logoJSH.svg" onerror="your.png">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>JSH</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon @click.native.stop="mini = !mini">
+                <v-toolbar-side-icon></v-toolbar-side-icon>
+              </v-btn>
+            </v-list-tile-action>
+
+          </v-list-tile>
+        </v-list>
+
+      </v-toolbar>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
+
+
+    <v-toolbar color="white" dark fixed app>
+      <v-toolbar-side-icon>
+        <v-icon dark left>arrow_back</v-icon>
+      </v-toolbar-side-icon>
+      <!--
+      <v-toolbar-title>Application</v-toolbar-title>
+    -->
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex text-xs-center>
+
+            <v-tooltip left>
+              <v-btn icon large target="_blank" slot="activator">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+
+            La vista del router se verá aqui
+
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+        mini: true,
+        right: null
+      }
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,46 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router';
+
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+// main.styl
+//import ('vuetify/src/stylus/main.styl') // Ensure you are using stylus-loader
+
+import Home from './components/Home.vue';
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [
+	{path: '/', component: Home},
+	{path: '/inicio', component: Home},
+	{path: '/bancos', component: Home},
+	{path: '/usuarios', component: Home},
+];
+
+const router = new VueRouter({
+	routes,
+	mode: 'history',
+	scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+		return savedPosition
+		return { x: 0, y: 0 }
+	} else {
+		return { x: 0, y: 0 }
+    }
+  },
+});
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
