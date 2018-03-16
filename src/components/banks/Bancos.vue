6749b847d8674f6b2f601d7b3b3c241544029301
Francisco Suárez <franksg88@gmail.com>
2018-03-16 14:33:53 -0600

    Se agrega mapa y redirección a edición

--- src/components/banks/Bancos.vue
+++ src/components/banks/Bancos.vue
@@ -6,107 +6,148 @@
       fluid>
       <v-flex 
         xs12 
-        m10 
-        lg8 
+        m10  
         offset-xs0 
-        offset-md1 
-        offset-lg2>
-        <div class="data-visualization-container">
-          <p class="module-title">
+        offset-md1>
+        <v-card>
+          <v-card-title>
             <v-icon>account_balance</v-icon>Banco de Alimentos
-          </p>
-          <div class="table">
-            <v-data-table
-              :headers="headers"
-              :items="items"
-              :loading="loading"
-              class="elevation-2">
-              <template  
-                slot="items" 
-                slot-scope="props">
-                <tr @click="props.expanded = !props.expanded">
-                  <td>
-                    <v-icon 
-                      :class="{
-                        'primary--text': props.item.habilitado,
-                        'error--text': !props.item.habilitado
-                    }">
-                      fiber_manual_record
-                    </v-icon>
-                  </td>
-                  <td>{{ props.item.nombre }}</td>
-                  <td>{{ props.item.region.nombre }}</td>
-                  <td>{{ props.item.direccion.estado }}</td>
-                  <td>{{ props.item.direccion.ciudad }}</td>
-                </tr>
-              </template>
-              <template 
-                slot="expand" 
-                slot-scope="props">
-                <v-container 
-                  fluid 
-                  grid-list-lg>
-                  <v-layout 
-                    row 
-                    wrap>
-                    <v-flex xs12>
-                      <v-list two-line>
-                        <v-list-tile>
-                          <v-list-tile-content>
-                            <v-list-tile-title>{{ props.item.razonSocial }}</v-list-tile-title>
-                            <v-list-tile-sub-title>Razón Social</v-list-tile-sub-title>
-                          </v-list-tile-content>
-                        </v-list-tile>
+            <v-spacer/>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Buscar"
+              single-line
+              hide-details
+            />
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            :search="search"
+            :loading="loading"
+            class="elevation-2">
+            <template  
+              slot="items" 
+              slot-scope="props">
+              <tr @click="props.expanded = !props.expanded">
+                <td>
+                  <v-icon 
+                    :class="{
+                      'primary--text': props.item.habilitado,
+                      'error--text': !props.item.habilitado
+                  }">
+                    fiber_manual_record
+                  </v-icon>
+                </td>
+                <td>{{ props.item.nombre }}</td>
+                <td>{{ props.item.region.nombre }}</td>
+                <td>{{ props.item.direccion.estado }}</td>
+                <td>{{ props.item.direccion.ciudad }}</td>
+              </tr>
+            </template>
+            <template 
+              slot="expand" 
+              slot-scope="props">
+              <v-container 
+                fluid 
+                justify-space-between
+                grid-list-lg>
+                <v-layout 
+                  row 
+                  wrap>
+                  <v-flex 
+                    xs12
+                    lg6>
+                    <v-list two-line>
+                      <v-list-tile>
+                        <v-list-tile-content>
+                          <v-list-tile-title>{{ props.item.razonSocial }}</v-list-tile-title>
+                          <v-list-tile-sub-title>Razón Social</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                      </v-list-tile>
 
-                        <v-list-tile>
-                          <v-list-tile-content>
-                            <v-list-tile-title>
-                              <star-rating
-                                :star-size="20"
-                                :read-only="true"
-                                :increment="0.01"
-                                :show-rating="false"
-                                v-model="props.item.calificacion"
-                              />
-                            </v-list-tile-title>
-                            <v-list-tile-sub-title>Calificación</v-list-tile-sub-title>
-                          </v-list-tile-content>
-                        </v-list-tile>
-                        <v-list-tile>
-                          <v-list-tile-content>
-                            <v-list-tile-title>
-                              {{ props.item.region.nombre }}
-                            </v-list-tile-title>
-                            <v-list-tile-sub-title>Región</v-list-tile-sub-title>
-                          </v-list-tile-content>
-                        </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>
+                          <v-list-tile-title>
+                            <star-rating
+                              :star-size="20"
+                              :read-only="true"
+                              :increment="0.01"
+                              :show-rating="false"
+                              v-model="props.item.calificacion"
+                            />
+                          </v-list-tile-title>
+                          <v-list-tile-sub-title>Calificación</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>
+                          <v-list-tile-title>
+                            {{ props.item.region.nombre }}
+                          </v-list-tile-title>
+                          <v-list-tile-sub-title>Región</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                      </v-list-tile>
 
-                        <v-list-tile>
-                          <v-list-tile-content>
-                            <v-list-tile-title>
-                              {{ props.item.fechaRegistro | moment("calendar") }}
-                            </v-list-tile-title>
-                            <v-list-tile-sub-title>Fecha de Registro</v-list-tile-sub-title>
-                          </v-list-tile-content>
-                        </v-list-tile>
-                      </v-list>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </template>
+                      <v-list-tile>
+                        <v-list-tile-content>
+                          <v-list-tile-title>
+                            {{ props.item.fechaRegistro | moment("calendar") }}
+                          </v-list-tile-title>
+                          <v-list-tile-sub-title>Fecha de Registro</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                      </v-list-tile>
+                    </v-list>
+                  </v-flex>
+                  <v-flex 
+                    xs12
+                    lg6>
+                    <gmap-map
+                      :center="{lat:props.item.direccion.latitude, lng:props.item.direccion.longitud}"
+                      :zoom="14"
+                      map-type-id="roadmap"
+                      style="width: 400px; height: 250px"
+                    >
+                      <gmap-marker
+                        :position="{lat:props.item.direccion.latitude, lng:props.item.direccion.longitud}"
+                        :clickable="true"
+                        :draggable="true"
+                      />
+                    </gmap-map>
+                    <blockquote class="subheading grey--text py-3" >
+                      {{ props.item.direccion.calle }} 
+                      #{{ props.item.direccion.numero }}. 
+                      {{ props.item.direccion.ciudad }},
+                      {{ props.item.direccion.estado }}.</blockquote>
+                  </v-flex>
+                </v-layout>
+                
+                <div class="text-xs-center">
+                  <v-btn
+                    outline 
+                    color="error">Eliminar</v-btn>
+                  <v-btn 
+                    :to="{ name: 'editorBanco', params: { id: props.item.id }}"
+                    outline 
+                    color="success">Editar</v-btn>
+                </div>
+              
+              </v-container>
+            </template>
 
-              <template slot="no-data">
-                <v-alert 
-                  :value="true" 
-                  outline 
-                  color="grey" 
-                  icon="info">
-                  No hay datos disponibles
-                </v-alert>
-              </template>
-            </v-data-table>
-          </div>
-        </div>
+            <template slot="no-data">
+              <v-alert 
+                :value="true" 
+                outline 
+                color="grey" 
+                icon="info">
+                No hay datos disponibles
+              </v-alert>
+            </template>
+          </v-data-table>
+          
+        </v-card>
         <v-btn 
           :to="'/nuevo-banco'"
           fixed 
@@ -134,7 +175,9 @@ export default {
     return {
       banksURL: "http://localhost:5000/api/bancos",
       loading: true,
+      search: "",
       items: [],
+      markers: [],
       errors: [],
       headers: [
         { text: "Estatus", value: "habilitado", sortable: false },
@@ -144,6 +187,12 @@ export default {
         { text: "Ciudad", value: "direccion.ciudad" }
       ]
     };
+  },
+  watch: {
+    $route(to, from) {
+      // Call resizePreserveCenter() on all maps
+      Vue.$gmapDefaultResizeBus.$emit("resize");
+    }
   },
   created() {
     this.getData();
@@ -165,17 +214,3 @@ export default {
   }
 };
 </script>
-
-<style scoped type="text/css">
-.module-title {
-  text-align: left !important;
-}
-
-.table {
-  width: 90%;
-}
-
-.data-visualization-container {
-  margin-left: 5%;
-}
-</style>
