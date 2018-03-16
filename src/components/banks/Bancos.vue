1552d6f44e5f4f74f52b89d1691d5f6b6dca8c08
Francisco Suárez <franksg88@gmail.com>
2018-03-16 13:07:46 -0600

    Se corrigen advertencias de Linter y Prettier

--- src/components/banks/Bancos.vue
+++ src/components/banks/Bancos.vue
@@ -1,7 +1,16 @@
 <template>
- <v-container grid-list-xl >
-    <v-layout row wrap fluid>
-      <v-flex xs12 m10 lg8 offset-xs0 offset-md1 offset-lg2>
+  <v-container grid-list-xl >
+    <v-layout 
+      row
+      wrap
+      fluid>
+      <v-flex 
+        xs12 
+        m10 
+        lg8 
+        offset-xs0 
+        offset-md1 
+        offset-lg2>
         <div class="data-visualization-container">
           <p class="module-title">
             <v-icon>account_balance</v-icon>Banco de Alimentos
@@ -12,13 +21,16 @@
               :items="items"
               :loading="loading"
               class="elevation-2">
-              <template slot="items" slot-scope="props">
+              <template  
+                slot="items" 
+                slot-scope="props">
                 <tr @click="props.expanded = !props.expanded">
                   <td>
-                    <v-icon v-bind:class="{
-                      'primary--text':  props.item.habilitado,
-                      'error--text': !props.item.habilitado
-                      }">
+                    <v-icon 
+                      :class="{
+                        'primary--text': props.item.habilitado,
+                        'error--text': !props.item.habilitado
+                    }">
                       fiber_manual_record
                     </v-icon>
                   </td>
@@ -28,126 +40,129 @@
                   <td>{{ props.item.direccion.ciudad }}</td>
                 </tr>
               </template>
-              <template slot="expand" slot-scope="props">
-                  <v-container fluid grid-list-lg>
-                    <v-layout row wrap>
-                      <v-flex xs12>
-                         <v-list two-line>
-                          <v-list-tile>
-                             <v-list-tile-content>
-                              <v-list-tile-title>{{ props.item.razonSocial }}</v-list-tile-title>
-                              <v-list-tile-sub-title>Razón Social</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                          </v-list-tile>
+              <template 
+                slot="expand" 
+                slot-scope="props">
+                <v-container 
+                  fluid 
+                  grid-list-lg>
+                  <v-layout 
+                    row 
+                    wrap>
+                    <v-flex xs12>
+                      <v-list two-line>
+                        <v-list-tile>
+                          <v-list-tile-content>
+                            <v-list-tile-title>{{ props.item.razonSocial }}</v-list-tile-title>
+                            <v-list-tile-sub-title>Razón Social</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
 
-                          <v-list-tile>
-                             <v-list-tile-content>
-                              <v-list-tile-title>
-                                <star-rating
-                                  :star-size="20"
-                                  :read-only="true"
-                                  :increment="0.01"
-                                  :show-rating="false"
-                                  v-model="props.item.calificacion"
-                                ></star-rating>
-                              </v-list-tile-title>
-                              <v-list-tile-sub-title>Calificación</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                          </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-content>
+                            <v-list-tile-title>
+                              <star-rating
+                                :star-size="20"
+                                :read-only="true"
+                                :increment="0.01"
+                                :show-rating="false"
+                                v-model="props.item.calificacion"
+                              />
+                            </v-list-tile-title>
+                            <v-list-tile-sub-title>Calificación</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-content>
+                            <v-list-tile-title>
+                              {{ props.item.region.nombre }}
+                            </v-list-tile-title>
+                            <v-list-tile-sub-title>Región</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
 
-                           <v-list-tile>
-                            <v-list-tile-content>
-                              <v-list-tile-title>
-                                {{ props.item.region.nombre }}
-                              </v-list-tile-title>
-                              <v-list-tile-sub-title>Región</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                          </v-list-tile>
-
-                           <v-list-tile>
-                             <v-list-tile-content>
-                              <v-list-tile-title>
-                                {{ props.item.fechaRegistro | moment("calendar") }}
-                              </v-list-tile-title>
-                              <v-list-tile-sub-title>Fecha de Registro</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                          </v-list-tile>
-                         </v-list>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
+                        <v-list-tile>
+                          <v-list-tile-content>
+                            <v-list-tile-title>
+                              {{ props.item.fechaRegistro | moment("calendar") }}
+                            </v-list-tile-title>
+                            <v-list-tile-sub-title>Fecha de Registro</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                        </v-list-tile>
+                      </v-list>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
               </template>
 
               <template slot="no-data">
-                <v-alert :value="true" outline color="grey" icon="info">
+                <v-alert 
+                  :value="true" 
+                  outline 
+                  color="grey" 
+                  icon="info">
                   No hay datos disponibles
                 </v-alert>
               </template>
             </v-data-table>
           </div>
         </div>
-        <v-btn fixed fab bottom right color="primary" :to="'/nuevo-banco'">
+        <v-btn 
+          :to="'/nuevo-banco'"
+          fixed 
+          fab 
+          bottom 
+          right 
+          color="primary">
           <v-icon>add</v-icon>
         </v-btn>
 
       </v-flex>
     </v-layout>
- </v-container>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios';
-import StarRating from 'vue-star-rating';
+import axios from "axios";
+import StarRating from "vue-star-rating";
 
 export default {
+  components: {
+    StarRating
+  },
   data() {
     return {
-      banksURL: 'http://localhost:5000/api/bancos',
+      banksURL: "http://localhost:5000/api/bancos",
       loading: true,
       items: [],
       errors: [],
       headers: [
-        { text: 'Estatus', value: 'habilitado', sortable: false },
-        { text: 'Nombre', value: 'nombre' },
-        { text: 'Región', value: 'region.nombre' },
-        { text: 'Estado', value: 'direccion.estado' },
-        { text: 'Ciudad', value: 'direccion.ciudad' },
-      ],
+        { text: "Estatus", value: "habilitado", sortable: false },
+        { text: "Nombre", value: "nombre" },
+        { text: "Región", value: "region.nombre" },
+        { text: "Estado", value: "direccion.estado" },
+        { text: "Ciudad", value: "direccion.ciudad" }
+      ]
     };
-  },
-  methods:{
-    getData(){
-      axios({
-        method: 'GET',
-        url: this.banksURL,
-      }).then((response) => {
-        this.items = response.data.data;
-        console.log(response.data.data);
-      }).catch((e) => {
-        this.errors.push(e);
-      });
-    }
   },
   created() {
     this.getData();
   },
-  components: {
-    StarRating,
-  },
-  created() {
-    axios({
-      method: 'GET',
-      url: this.banksURL,
-    })
-      .then((response) => {
-        this.items = response.data.data;
-        this.loading = false;
+  methods: {
+    getData() {
+      axios({
+        method: "GET",
+        url: this.banksURL
       })
-      .catch((e) => {
-        this.errors.push(e);
-        console.log(this.errors);
-      });
-  },
+        .then(response => {
+          this.items = response.data.data;
+          this.loading = false;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    }
+  }
 };
 </script>
 
--- src/components/users/Usuarios.vue
+++ src/components/users/Usuarios.vue
@@ -36,57 +36,62 @@
 </template>
 
 <script>
-  import axios from "axios";
+import axios from "axios";
 
-  export default {
-    data () {
-      return {
-        usersURL: "http://localhost:5000/api/usuarios",
-        items: [],
-        errors: [],
-        headers: [
-          { text: 'Nombre', value: 'nombre' },
-          { text: 'Tipo', value: 'tipoUsuario.tipo' },
-          { text: 'Curso Social', value: 'curso.nombre' },
-          { text: 'Grupo', value: 'grupo' },
-          { text: 'Ciudad', value: 'direccion.ciudad' },
-        ],
-      }
-    },
-    methods:{
-      getData(){
-        axios({
-          method: 'GET',
-          url: this.usersURL,
-        }).then((response) => {
+export default {
+  data() {
+    return {
+      usersURL: "http://localhost:5000/api/usuarios",
+      items: [],
+      errors: [],
+      headers: [
+        { text: "Nombre", value: "nombre" },
+        { text: "Tipo", value: "tipoUsuario.tipo" },
+        { text: "Curso Social", value: "curso.nombre" },
+        { text: "Grupo", value: "grupo" },
+        { text: "Ciudad", value: "direccion.ciudad" }
+      ]
+    };
+  },
+  methods: {
+    getData() {
+      axios({
+        method: "GET",
+        url: this.usersURL
+      })
+        .then(response => {
           this.items = response.data.data;
           //console.log(response.data.data);
-        }).catch((e) => {
+        })
+        .catch(e => {
           this.errors.push(e);
         });
-      },
-      editItem(item){
-        console.log(this._routerRoot);
-        //:to="{ name: 'editorUsuario', params: { id: item.id }}"
-        this._routerRoot._router.push({ name: 'editorUsuario', params: { id: item.id }});        
-      }
     },
-    created() {
-      this.getData();
-    },
+    editItem(item) {
+      console.log(this._routerRoot);
+      //:to="{ name: 'editorUsuario', params: { id: item.id }}"
+      this._routerRoot._router.push({
+        name: "editorUsuario",
+        params: { id: item.id }
+      });
+    }
+  },
+  created() {
+    this.getData();
   }
+};
 </script>
 
 <style scoped type="text/css">
-  .module-title{
-    text-align: left !important;
-  }
+.module-title {
+  text-align: left !important;
+}
 
-  .table{
-    width: 90%;
-  }
+.table {
+  width: 90%;
+}
 
-  .data-visualization-container{
-    margin-left: 5%;
-  }
+.data-visualization-container {
+  margin-left: 5%;
+}
 </style>
