44b4c1da1c4ebfa738c1ca04cd0d2ff499e83251
Isaac <DMDovahkiin@gmail.com>
2018-03-16 19:29:09 -0600

    Users have now expandable tables

--- src/components/users/Usuarios.vue
+++ src/components/users/Usuarios.vue
@@ -6,25 +6,124 @@
         <v-icon>account_balance</v-icon>Usuarios
       </p>
       <div class="table">
+
         <v-data-table
           :headers="headers"
           :items="items"
           class="elevation-2">
           <template slot="items" slot-scope="props">
-            <tr v-on:click="editItem(props.item)">
+            <tr @click="props.expanded = !props.expanded">
               <td>{{ props.item.nombre }}</td>
               <td>{{ props.item.tipoUsuario.tipo }}</td>
               <td>{{ props.item.curso.nombre }}</td>
               <td>{{ props.item.grupo }}</td>
               <td>{{ props.item.direccion.ciudad }}</td>
-            </tr>
+            </tr>          
+          </template>
+
+          <template 
+            slot="expand" 
+            slot-scope="props">
+            <v-container 
+              fluid 
+              justify-space-between
+              grid-list-lg>
+              <v-layout 
+                row 
+                wrap>
+                <v-flex 
+                  xs12
+                  lg6>
+                  <v-list two-line>
+
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>
+                          {{props.item.nombre}}
+                        </v-list-tile-title>
+                        <v-list-tile-sub-title>{{ props.item.tipoUsuario.tipo }}</v-list-tile-sub-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>
+                          Grupo
+                        </v-list-tile-title>
+                        <v-list-tile-sub-title>{{ props.item.grupo }}</v-list-tile-sub-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>
+                          Último inicio de sesión
+                        </v-list-tile-title>
+                        <v-list-tile-sub-title>{{ props.item.ultimoLogin | moment("calendar")}}</v-list-tile-sub-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+
+                  </v-list>
+                </v-flex>
+
+                <v-flex 
+                  xs12
+                  lg6>
+                  <v-list three-line>
+
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>
+                          Curso: {{ props.item.curso.nombre}}
+                        </v-list-tile-title>
+                        <v-list-tile-sub-title v-if="props.item.curso.calificacion">
+                          Calificación: {{ props.item.curso.calificacion}}/10
+                        </v-list-tile-sub-title>
+                        <v-list-tile-sub-title v-if="props.item.curso.descripcion">
+                          Descripción: {{ props.item.curso.descripcion}}
+                        </v-list-tile-sub-title>
+                        <v-list-tile-sub-title v-if="props.item.curso.observaciones">
+                          Observaciones: {{ props.item.curso.observaciones}}
+                        </v-list-tile-sub-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>
+                          Dirección
+                        </v-list-tile-title>
+                        <v-list-tile-sub-title>{{ props.item.direccion.estado }} </v-list-tile-sub-title>
+                        <v-list-tile-sub-title>{{ props.item.direccion.ciudad }} </v-list-tile-sub-title>
+                        <v-list-tile-sub-title>{{ props.item.direccion.cp }} </v-list-tile-sub-title>
+                        <v-list-tile-sub-title>{{ props.item.direccion.calle }} </v-list-tile-sub-title>                        
+                      </v-list-tile-content>                      
+                    </v-list-tile>
+
+                  </v-list>
+                </v-flex>
+              </v-layout>
+              
+              <div class="text-xs-center">
+                <v-btn
+                  outline 
+                  color="error">Eliminar</v-btn>
+                <v-btn 
+                  :to="{ name: 'editorUsuario', params: { id: props.item.id }}"
+                  outline 
+                  color="success">Editar</v-btn>
+              </div>
+            
+            </v-container>
           </template>
         </v-data-table>
+
         <ul v-if="errors && errors.length">
           <li v-bind:key="error" v-for="error of errors">
             {{error.message}}
           </li>
         </ul>
+
       </div>
     </div>
 
@@ -68,8 +167,6 @@ export default {
         });
     },
     editItem(item) {
-      console.log(this._routerRoot);
-      //:to="{ name: 'editorUsuario', params: { id: item.id }}"
       this._routerRoot._router.push({
         name: "editorUsuario",
         params: { id: item.id }
