5c80537ed9531120f718f31ae7c8258c6e2ef717
isaac2 <isaac.morelosc@gmail.com>
2018-08-08 16:52:34 -0500

    Endpoints were not changed in last commit. Fixed

--- src/configs/apiRoutes.js
+++ src/configs/apiRoutes.js
@@ -5,29 +5,29 @@ export const apiRoutes = {
     Accept: "application/json"
   },
   testing: {
-    allBanksURL: "http://localhost:5000/api/all/bancoalimentos",
-    allRegionsURL: "http://localhost:5000/api/all/region",
+    allBanksURL: "http://18.221.249.12:5000/api/all/bancoalimentos",
+    allRegionsURL: "http://18.221.249.12:5000/api/all/region",
 
-    addressURL: "http://localhost:5000/api/direccion/IDDireccion",
-    bankURL: "http://localhost:5000/api/bancoalimentos/IDBancoAlimentos",
-    contactURL: "http://localhost:5000/api/contacto/IDContacto",
-    regionURL: "http://localhost:5000/api/region/IDRegion",
+    addressURL: "http://18.221.249.12:5000/api/direccion/IDDireccion",
+    bankURL: "http://18.221.249.12:5000/api/bancoalimentos/IDBancoAlimentos",
+    contactURL: "http://18.221.249.12:5000/api/contacto/IDContacto",
+    regionURL: "http://18.221.249.12:5000/api/region/IDRegion",
 
-    allGruposURL: "http://localhost:5000/api/grupos",
-    allGruposGraduadosURL: "http://localhost:5000/api/gruposGraduados",
+    allGruposURL: "http://18.221.249.12:5000/api/grupos",
+    allGruposGraduadosURL: "http://18.221.249.12:5000/api/gruposGraduados",
 
-    allCommunitaryCentersURL: "http://localhost:5000/api/centros",
-    allJobsURL: "http://localhost:5000/api/empleos",
-    allServicesURL: "http://localhost:5000/api/servicios",
+    allCommunitaryCentersURL: "http://18.221.249.12:5000/api/centros",
+    allJobsURL: "http://18.221.249.12:5000/api/empleos",
+    allServicesURL: "http://18.221.249.12:5000/api/servicios",
 
     //Personas
-    allTrabajadoresURL: "http://localhost:5000/api/trabajadores",
-    allMentoresURL: "http://localhost:5000/api/mentores",
-    allFamiliasURL: "http://localhost:5000/api/familias",
-    allAliadosURL: "http://localhost:5000/api/aliados",
-    allTutoresURL: "http://localhost:5000/api/tutores",
+    allTrabajadoresURL: "http://18.221.249.12:5000/api/trabajadores",
+    allMentoresURL: "http://18.221.249.12:5000/api/mentores",
+    allFamiliasURL: "http://18.221.249.12:5000/api/familias",
+    allAliadosURL: "http://18.221.249.12:5000/api/aliados",
+    allTutoresURL: "http://18.221.249.12:5000/api/tutores",
 
-    allBeneficiariosURL: "http://localhost:5000/api/beneficiarios"
+    allBeneficiariosURL: "http://18.221.249.12:5000/api/beneficiarios"
   },
   jsh: {
     allBanksURL: "http://18.221.249.12:80/api/bancoalimentos",
